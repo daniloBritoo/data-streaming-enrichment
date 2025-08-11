efb4f9baa3d284d519f9ee76a328a758f887f94b
daniloBritoo <danilo.brito@sptech.school>
2025-08-11 18:39:01 -0300

    feat/add_ecommerce_producer

---
+++ producer/producer.py
@@ -0,0 +1,22 @@
+from kafka import KafkaProducer
+import json, random, time
+
+
+def generate_buy_event():
+    p = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+    status = ['APROVADO', 'PENDENTE', 'NEGADO']
+    while True:
+
+        data = {
+            "usuario_id": random.randint(1,10),
+            "produto_id": random.randint(1,10),
+            "status": random.choice(status),
+            "quantidade": random.randint(1,50)
+        }
+        p.send('ecommerce_buys', data)
+
+        p.flush()
+        print("evento enviado")
+        time.sleep(1)
+
+generate_buy_event()
